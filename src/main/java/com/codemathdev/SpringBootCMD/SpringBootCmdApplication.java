c15ad4806fbc00c68326bf3a7d18fe2d35ca6ce1
Rahmat Prastyo <rahmatprastyo88@gmail.com>
2020-06-26 15:22:57 +0700

    first commit

---
+++ src/main/java/com/codemathdev/SpringBootCMD/SpringBootCmdApplication.java
@@ -0,0 +1,13 @@
+package com.codemathdev.SpringBootCMD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCmdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codemathdev/SpringBootCMD/SpringBootCmdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemathdev.SpringBootCMD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCmdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
