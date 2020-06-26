093ff99ae7a571765e19d616a49911a79e920b9d
Rahmat Prastyo <rahmatprastyo88@gmail.com>
2020-06-26 15:53:01 +0700

    menambahkan url /home

---
+++ src/main/java/com/codemathdev/SpringBootCMD/controller/mvc/Home.java
@@ -0,0 +1,15 @@
+package com.codemathdev.SpringBootCMD.controller.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class Home {
+
+    @GetMapping
+    public String home() {
+        return "Home";
+    }
+}
